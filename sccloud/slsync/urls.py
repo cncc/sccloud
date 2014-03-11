d098e5a167353af3ec15fbfa7f95d82c0c097c8b
cc <ccissc@cn.ibm.com>
2014-03-11 16:04:32 +0800

    to github

--- sccloud/slsync/urls.py
+++ sccloud/slsync/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from slsync import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
-    url(r'^200\/$', views.detail, name='detail')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<inst_id>\d+)/$', views.detail, name='detail'),
 )
 
--- sccloud/slsync/views.py
+++ sccloud/slsync/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from slsync.models import Inst
@@ -8,7 +9,7 @@ def index(request):
     template = loader.get_template('slsync/index.html')
     context = RequestContext(request, {'all_inst_list': all_inst_list,})
     return HttpResponse(template.render(context))
-    return HttpResponse(output)
 
 def detail(request, inst_id):
-    return HttpResponse("you are looking for: "%inst_id)
+    inst_detail = get_object_or_404(Inst,pk=inst_id)
+    return render(request,'slsync/detail.html',{'inst': inst_detail})
